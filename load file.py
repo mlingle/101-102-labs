51e6b9d56c90ca999cc39f665ed57a041f470fea
mlingle <74617480+mlingle@users.noreply.github.com>
2020-11-30 15:15:54 -0700

    Update load file.py

--- load file.py
+++ load file.py
@@ -1,3 +1,8 @@
+#   Matthew Lingle
+#   CSCI 102 – Section C
+#   Week 12 - Utility using Git and Incremental Development
+#   References: Jack
+#   Time: 45 minutes
 def load_file(file_name):
     with open(file_name,"r") as file:
         return file.readlines()
